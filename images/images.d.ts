ec66204930dbd9d38b67709d3d585969eea966d9
Thibaut Sardan <github@thib.top>
2021-09-23 12:26:15 +0200

    Updates

---
+++ images/images.d.ts
@@ -0,0 +1,19 @@
+declare module "*.svg" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.png" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.woff" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.woff2" {
+  const url: string;
+  export default url;
+}
